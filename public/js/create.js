0ac7f886269648095c3a402d9c4acd81046f7cb0
musicplayer0 <64247017+lequan81@users.noreply.github.com>
2022-05-29 20:21:32 +0700

    first commit

---
+++ public/js/create.js
@@ -0,0 +1,21 @@
+// javascript for create.html
+const form = document.querySelector('form');
+
+const createPost = async (e) => {
+  e.preventDefault();
+
+  const doc = {
+    url: form.url.value,
+    direction: form.direction.value,
+  }
+
+  await fetch('http://localhost:3001/imgs', {
+    method: 'POST',
+    body: JSON.stringify(doc),
+    headers: { 'Content-Type': 'application/json' }
+  })
+
+  window.location.replace('/')
+}
+
+form.addEventListener('submit', createPost);
---
+++ public/js/index.js
@@ -0,0 +1,38 @@
+// javascript for index.html
+const container = document.querySelector('.blogs');
+const searchForm = document.querySelector('.search');
+
+const renderPosts = async (term) => {
+  console.log(term);
+  let uri = 'http://localhost:3001/imgs?_sort=id';
+  if (term) {
+    uri += `&q=${term}`
+  }
+
+  const res = await fetch(uri);
+  const posts = await res.json();
+  console.log(posts);
+
+  let template = '';
+  posts.forEach(post => {
+    template += `
+    <div class="text-center mt-6 mx-8 p-6 sm:py-6 rounded-lg shadow-xl bg-white">
+      <h2 class="text-xl text-gray-700 font-bold mb-2">${post.url}</h2>
+        <p class="text-lg text-gray-400 font-normal">${post.direction}</p>
+        <a href="/details?id=${post.id}" class="text-green-500 no-underline font-semibold">
+          Read more
+        </a>
+    </div>
+    `
+  });
+
+  container.innerHTML = template;
+}
+
+// search
+searchForm.addEventListener('submit', async (e) => {
+  e.preventDefault();
+  renderPosts(searchForm.term.value.trim());
+})
+
+window.addEventListener('DOMContentLoaded', () => renderPosts());
